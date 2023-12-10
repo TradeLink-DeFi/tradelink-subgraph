185d68ab1a88ff23d081d2a42d1af1a68eaa981c
Supanut1911 <ynotmela@gmail.com>
2023-12-10 11:38:33 +0700

    Initialize subgraph

--- v2tradelink-trade-statue-cker/src/trade-link-ccipv-1.ts
+++ v2tradelink-trade-statue-cker/src/trade-link-ccipv-1.ts
@@ -1,13 +1,45 @@
 import {
+  CreateFulfill as CreateFulfillEvent,
+  CreateOffer as CreateOfferEvent,
   OwnershipTransferRequested as OwnershipTransferRequestedEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
   Success as SuccessEvent
 } from "../generated/TradeLinkCCIPV1/TradeLinkCCIPV1"
 import {
+  CreateFulfill,
+  CreateOffer,
   OwnershipTransferRequested,
   OwnershipTransferred,
   Success
 } from "../generated/schema"
+
+export function handleCreateFulfill(event: CreateFulfillEvent): void {
+  let entity = new CreateFulfill(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.fulfillId = event.params.fulfillId
+  entity.ownerFulfill = event.params.ownerFulfill
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleCreateOffer(event: CreateOfferEvent): void {
+  let entity = new CreateOffer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.offerId = event.params.offerId
+  entity.ownerOffer = event.params.ownerOffer
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
 
 export function handleOwnershipTransferRequested(
   event: OwnershipTransferRequestedEvent
--- v2tradelink-trade-statue-cker/tests/trade-link-ccipv-1-utils.ts
+++ v2tradelink-trade-statue-cker/tests/trade-link-ccipv-1-utils.ts
@@ -1,10 +1,60 @@
 import { newMockEvent } from "matchstick-as"
-import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import { ethereum, BigInt, Address } from "@graphprotocol/graph-ts"
 import {
+  CreateFulfill,
+  CreateOffer,
   OwnershipTransferRequested,
   OwnershipTransferred,
   Success
 } from "../generated/TradeLinkCCIPV1/TradeLinkCCIPV1"
+
+export function createCreateFulfillEvent(
+  fulfillId: BigInt,
+  ownerFulfill: Address
+): CreateFulfill {
+  let createFulfillEvent = changetype<CreateFulfill>(newMockEvent())
+
+  createFulfillEvent.parameters = new Array()
+
+  createFulfillEvent.parameters.push(
+    new ethereum.EventParam(
+      "fulfillId",
+      ethereum.Value.fromUnsignedBigInt(fulfillId)
+    )
+  )
+  createFulfillEvent.parameters.push(
+    new ethereum.EventParam(
+      "ownerFulfill",
+      ethereum.Value.fromAddress(ownerFulfill)
+    )
+  )
+
+  return createFulfillEvent
+}
+
+export function createCreateOfferEvent(
+  offerId: BigInt,
+  ownerOffer: Address
+): CreateOffer {
+  let createOfferEvent = changetype<CreateOffer>(newMockEvent())
+
+  createOfferEvent.parameters = new Array()
+
+  createOfferEvent.parameters.push(
+    new ethereum.EventParam(
+      "offerId",
+      ethereum.Value.fromUnsignedBigInt(offerId)
+    )
+  )
+  createOfferEvent.parameters.push(
+    new ethereum.EventParam(
+      "ownerOffer",
+      ethereum.Value.fromAddress(ownerOffer)
+    )
+  )
+
+  return createOfferEvent
+}
 
 export function createOwnershipTransferRequestedEvent(
   from: Address,
